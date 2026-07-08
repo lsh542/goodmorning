c34b2e72f6b644374588d318a46ee2843649dc70
林少华 <279643160@qq.com>
2026-07-08 13:39:52 +0800

    Fix data dictionary structure in main.py

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
 wea, temperature = get_weather()
-data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"birthday_left_d":{"value":get_birthday_d()},"words":{"value":get_words(), "birthday_left_dd":get_birthday_dd()}}
+data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"birthday_left_d":{"value":get_birthday_d()},"words":{"value":get_words()},"birthday_left_dd":{"value":get_birthday_dd()}}
 print(data)
 res = wm.send_template(user_id, template_id, data)
 print(res)
